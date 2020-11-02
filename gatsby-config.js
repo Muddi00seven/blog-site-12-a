b26149b6d6879ed0231ed5c89b60826640874874
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-11-02 15:54:10 +0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-typescript",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `a7arp11gonu9`,
+        accessToken: `3YsgBq9rfaJM2U5z_f8UFg8XJDC1Ea5Ll2WKkToaLBk`,
+      },
+    },
+
+    `@contentful/gatsby-transformer-contentful-richtext`,
+  ],
+};
